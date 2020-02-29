91465e675d45c21d2125c716c74167ce26f1fc6e
Linus Kvarnhammar <31400627+lkvarnhammar@users.noreply.github.com>
2020-02-29 15:41:30 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 echo hej
+apt update
+apt upgrade -y
+curl -sL https://deb.nodesource.com/setup_13.x | sudo bash -
+apt install -y nodejs
+echo "Node version installed:"
+node -v
+apt install gcc g++ make
+npm install
+npm start
